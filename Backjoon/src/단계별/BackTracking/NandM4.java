768d1e02cd88d283f0f019f8046ced9218a79386
Hansuk.YU <yusw10@naver.com>
2020-08-15 21:52:12 +0900

    algo study

--- Backjoon/src/단계별/BackTracking/NandM4.java
+++ Backjoon/src/단계별/BackTracking/NandM4.java
@@ -22,23 +22,24 @@ public class NandM4 {
 		int N = Integer.parseInt(tokens.nextToken());
 		int M = Integer.parseInt(tokens.nextToken());
 
-		dfs(N,M, 0, new int[M]);
+		dfs(N, 1, 0, new int[M]);
 		System.out.println(sb);
 
 	}
 
-	private static void dfs(int n, int m,int current, int[] temp) {
-		if (current == m) {
+	private static void dfs(int n, int num, int current, int[] temp) {
+		if (current == temp.length) {
 			for (int item : temp) {
 				sb.append(item + " ");
 			}
 			sb.append("\n");
-		} else {
-			for (int i = current; i <= n; i++) {
-				temp[current] = i+1;
-				dfs(n, m, current +1, temp);
+		} else if(num > n) {
+			return;
+		}else {
+			for(int i = num; i <= n; i++) {
+				temp[current] = i ; 
+				dfs(n, num++, current+ 1, temp);
 			}
-
 		}
 	}
 
